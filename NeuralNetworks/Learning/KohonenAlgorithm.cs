7064382777072a52a83bae30fefb8f77daf6d9a6
Mateusz Gałasiński <210183@edu.p.lodz.pl>
2018-05-11 01:28:13 +0200

    Conscience, Lambda patches, Learning rate adaption

--- NeuralNetworks/Learning/KohonenAlgorithm.cs
+++ NeuralNetworks/Learning/KohonenAlgorithm.cs
@@ -5,76 +5,96 @@ using System.Text;
 using System.Threading.Tasks;
 using MathNet.Numerics.LinearAlgebra;
 using NeuralNetworks.DistanceMetrics;
+using NeuralNetworks.Learning.MLP;
 using NeuralNetworks.Learning.NeighborhoodFunctions;
 
 namespace NeuralNetworks.Learning
 {
     public class KohonenAlgorithm : SONLearningAlgorithm
     {
-        public KohonenAlgorithm(double learningRate, ILengthCalculator lengthCalculator, Lambda borderParameter, INeighborhoodFunction neighborhoodFunction) : base(learningRate, lengthCalculator)
+        public KohonenAlgorithm(
+            SONLearningRateHandler learningRateHandler,
+            ILengthCalculator lengthCalculator,
+            Lambda borderParameter,
+            INeighborhoodFunction neighborhoodFunction, 
+            ConscienceWithPotential conscience) : base(learningRateHandler, lengthCalculator)
         {
             BorderParameter = borderParameter;
             NeighborhoodFunction = neighborhoodFunction;
+            Conscience = conscience;
         }
 
         /// <summary>
         /// lambda
         /// </summary>
-        public Lambda BorderParameter { get; set; }
+        public Lambda BorderParameter { get; }
 
-        public INeighborhoodFunction NeighborhoodFunction { get; set; }
+        public INeighborhoodFunction NeighborhoodFunction { get; }
+
+        public ConscienceWithPotential Conscience { get;}
 
         private int iterationNumber = 0;
 
         public override void AdaptWeights(NeuralNetwork network, Vector<double> learningPoint)
         {
-            var LC = LengthCalculator; // to shorten
             var weights = network.Layers[0].Weights;
+
             // find winner neuron
-            int winnerIndex = 0; // assume first neuron is the winner
-            double winnerDistance = LC.Distance(learningPoint, weights.Column(winnerIndex));
-            for (int colIndex = 1; colIndex < weights.ColumnCount; colIndex++)
+            int winnerIndex = FindWinnerIndex(weights, learningPoint, LengthCalculator);
+               
+            // find neighbors
+            var winner = weights.Column(winnerIndex);
+            var neighborsGrade = new Dictionary<int, double>();
+            for (int colIndex = 0; colIndex < weights.ColumnCount; colIndex++)
+            {
+                var colDistance = LengthCalculator.Distance(weights.Column(colIndex), winner);   
+                neighborsGrade.Add(colIndex, NeighborhoodFunction.Calculate(colDistance, BorderParameter.GetValue(iterationNumber)));
+            }
+            // adapt winner and neighbors weights
+            UpdateNeurons(learningPoint, weights, neighborsGrade);
+            Conscience.UpdatePotential(winnerIndex);
+
+            iterationNumber++;
+        }
+
+        private int FindWinnerIndex(Matrix<double> weights, Vector<double> learningPoint, ILengthCalculator calculator)
+        {
+            int winnerIndex = new Random().Next(0,weights.ColumnCount); // assume some neuron is the winner
+            double winnerDistance = LengthCalculator.Distance(learningPoint, weights.Column(winnerIndex));
+            for (int colIndex = 0; colIndex < weights.ColumnCount; colIndex++)
             {
                 if (IsBetter(colIndex))
                 {
-                    UpdateWinnerIndex(colIndex);
+                    if (Conscience.CanBeWinner(colIndex))
+                    {
+                        UpdateWinnerIndex(colIndex);
+                    }
                 }
             }
-            // adapt winners weights
-            var winner = weights.Column(winnerIndex);
-            var neighborsIndexes = new List<int>();
-            for (int colIndex = 0; colIndex < weights.ColumnCount; colIndex++)
-            {
-                var colDistance = LC.Distance(weights.Column(colIndex), winner);
-                if (colDistance < BorderParameter.GetValue(iterationNumber))
-                {
-                    neighborsIndexes.Add(colIndex);
-                }
-            }
-            foreach (var neuronIndex in neighborsIndexes)
-            {
-                UpdateNeuron(learningPoint, weights, neuronIndex);
-            }
-            iterationNumber++;
+            return winnerIndex;
 
-            //local methods
+            #region local methods
             bool IsBetter(int neuronIndex)
             {
-                return LC.Distance(learningPoint, weights.Column(neuronIndex)) < winnerDistance;
+                return LengthCalculator.Distance(learningPoint, weights.Column(neuronIndex)) < winnerDistance;
             }
             void UpdateWinnerIndex(int newWinnerIndex)
             {
                 winnerIndex = newWinnerIndex; // assume first neuron is the winner
-                winnerDistance = LC.Distance(learningPoint, weights.Column(winnerIndex));
+                winnerDistance = calculator.Distance(learningPoint, weights.Column(winnerIndex));
             }
+            #endregion
         }
 
-        private void UpdateNeuron(Vector<double> learningPoint, Matrix<double> weights, int neuronIndex)
+        private void UpdateNeurons(Vector<double> learningPoint, Matrix<double> weights, Dictionary<int, double> neighborsGrade)
         {
-            Vector<double> correctionVector = LearningRate * (learningPoint - weights.Column(neuronIndex));
-            for (int i = 0; i < correctionVector.Count; i++)
+            foreach (var neuronIndex in neighborsGrade.Keys)
             {
-                weights[i, neuronIndex] += correctionVector[i];
+                Vector<double> correctionVector = LearningRateHandler.GetLearningRate(iterationNumber) * (learningPoint - weights.Column(neuronIndex));
+                for (int i = 0; i < correctionVector.Count; i++)
+                {
+                    weights[i, neuronIndex] += neighborsGrade[neuronIndex] * correctionVector[i];
+                }
             }
         }
     }
